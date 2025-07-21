c72295ce4babbcce4de971b33458bfc1ba218211
narenr94 <narenr94@gmail.com>
2025-07-21 15:01:03 +0530

    Move PluginsTests

---
+++ test/utests/tests/PluginsTests/PlayerVeriMatrixDecryptorTests.cpp
@@ -0,0 +1,44 @@
+/*
+ * If not stated otherwise in this file or this component's license file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2025 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "gstverimatrixdecryptor.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+class PluginsTests : public ::testing::Test
+{
+protected:
+	//GstAampverimatrixdecryptor *aampverimatrixdecryptor;
+	
+	void SetUp() override
+	{
+		//aampverimatrixdecryptor = new GstAampverimatrixdecryptor();
+	}
+
+	void TearDown() override
+	{
+		//delete aampverimatrixdecryptor;
+	}
+public:
+};
+
+TEST_F(PluginsTests, TestVeriMatrixDecryptor_Init)
+{
+	//GstAampverimatrixdecryptor *aampverimatrixdecryptor = NULL;
+	//gst_aampverimatrixdecryptor_init(aampverimatrixdecryptor);
+}
